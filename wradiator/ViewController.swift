9b9492c54973cdee48f4bdcda90993ac69c9bd49
Ben Ward <ben@benward.me>
2017-07-01 00:54:54 -0700

    Initial Commit

---
+++ wradiator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  wradiator
+//
+//  Created by Ben Ward on 7/1/17.
+//  Copyright © 2017 Ben Ward. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
